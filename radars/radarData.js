d10217671702cf3b3b7fbba65152b71f060aa3c6
suntong <suntong@midu.com>
2019-05-06 00:36:12 +0800

    增加最近读的6本书

--- radars/radarData.js
+++ radars/radarData.js
@@ -19,7 +19,7 @@ var radar_arcs = [
 //   t = moved
 //   c = stayed put
 //
-// blipSize: 
+// blipSize:
 //  intValue; This is optional, if you omit this property, then your blip will be size 70.
 //            This give you the ability to be able to indicate information by blip size too
 //
@@ -53,19 +53,22 @@ var radar_data = [
         "left" : 45,
         "top" : 18,
         "color" : "#8FA227",
-        "items" : [ 
+        "items" : [
             {"name": "原则", pc: { r: 50, t: 133 }, "movement":"c" },
-{"name":"人性的弱点", "pc":{"r":60,"t":120},"movement":"c"},
+            {"name":"人性的弱点", "pc":{"r":60,"t":120},"movement":"c"},
             {"name":"深度工作", "pc":{"r":70,"t":165},"movement":"c"},
             {"name":"思考，快与慢", "pc":{"r":80,"t":120},"movement":"c"},
             {"name":"黑天鹅", "pc":{"r":90,"t":100},"movement":"c"},
-			{"name":"自学是门艺术", "pc":{"r":160,"t":130},"movement":"c"},
+		        {"name":"自学是门艺术", "pc":{"r":160,"t":130},"movement":"c"},
             {"name":"巨人的工具", "pc":{"r":310,"t":135},"movement":"c"},
             {"name":"穷查理宝典", "pc":{"r":320,"t":100},"movement":"c"},
             {"name":"增长黑客", "pc":{"r":330,"t":170},"movement":"c"},
             {"name":"说话之道", "pc":{"r":340,"t":120},"movement":"c"},
             {"name":"人性的弱点", "pc":{"r":350,"t":150},"movement":"c"},
             {"name":"弱传播", "pc":{"r":260,"t":130},"movement":"c"},
+            {"name":"成功的真谛", "pc":{"r":110,"t":160},"movement":"c"},
+            {"name":"20岁,光阴不再来", "pc":{"r":115,"t":130},"movement":"c"},
+            {"name":"高敏感是种天赋", "pc":{"r":135,"t":150},"movement":"c"},
             {"name":"思辨与立场", "pc":{"r":380,"t":140},"movement":"c"},
             {"name":"从0到1", "pc":{"r":370,"t":100},"movement":"c"}
         ]
@@ -75,11 +78,13 @@ var radar_data = [
         "top" : 18,
         "color" : "#587486",
         "items" : [
-            {"name": "自卑与超越", pc: { r: 110, t: 45 }, "movement":"c" },
+            {"name":"自卑与超越", pc: { r: 110, t: 45 }, "movement":"c" },
+            {"name":"如何让你爱的人爱上你", pc: { r: 150, t: 65 }, "movement":"c" },
             {"name":"千面英雄", "pc":{"r":330,"t":75},"movement":"c"},
             {"name":"活出生命的意义", "pc":{"r":350,"t":25},"movement":"c"},
             {"name":"瓦尔登湖", "pc":{"r":10,"t":45},"movement":"c"},
-            {"name":"乌合之众", "pc":{"r":380,"t":35},"movement":"c"}
+            {"name":"乌合之众", "pc":{"r":380,"t":35},"movement":"c"},
+            {"name":"世界上最神奇的24堂课", pc: { r: 90, t: 60 }, "movement":"c" }
   ]
     },
     { "quadrant": "小说与文学",
@@ -90,7 +95,7 @@ var radar_data = [
             {"name": "活着", pc: { r: 40, t: 225 }, "movement":"c" },
             {"name":"我是你爸爸", "pc":{"r":140,"t":200},"movement":"c"},
             {"name":"变身", "pc":{"r":180,"t":220},"movement":"c"},
-            {"name":"流浪者之歌", "pc":{"r":330,"t":260},"movement":"c"},
+            {"name":"流浪者之歌", "pc":{"r":50,"t":260},"movement":"c"},
             {"name":"傲慢与偏见", "pc":{"r":320,"t":200},"movement":"c"},
             {"name":"围城", "pc":{"r":350,"t":220},"movement":"c"}
         ]
